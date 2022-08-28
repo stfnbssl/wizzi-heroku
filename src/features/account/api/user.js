3cb629b449fecc11ed10898ef62a9499e47470c2
stfnbssl <stfn.bssl@gmail.com>
2022-08-28 22:09:01 +0200

    try 31

--- src/features/account/api/user.js
+++ src/features/account/api/user.js
@@ -5,7 +5,7 @@ const tslib_1 = require("tslib");
 /*
     artifact generator: C:\My\wizzi\stfnbssl\wizzi\packages\wizzi-js\lib\artifacts\ts\module\gen\main.js
     package: wizzi-js@0.7.11
-    primary source IttfDocument: C:\My\wizzi\stfnbssl\wizzi.meta.demos\packages\wizzi-heroku\.wizzi\src\features\account\api\user.ts.ittf
+    primary source IttfDocument: C:\My\wizzi\stfnbssl\wizzi.meta.demos\packages\wizzi-heroku\.wizzi\src\features\account\api\user.tsx.ittf
 */
 const user_1 = require("../mongo/user");
 const myname = 'features.account.api.user';
